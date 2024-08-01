7539cca45a4c880f54eddd8a7037f4450a126a31
Hyu Okumura <Hyu.Okumura@ibm.com>
2024-08-01 12:04:03 +0900

    add: mockデータ

---
+++ src/mock/MockSearchResult.js
@@ -0,0 +1,29 @@
+export const MockSearchResult = {
+    result: [
+        {
+        filename: "filenameです。1",
+        passage: "title=%E3%82%B3%E3%83%AD%E3%83%8A%E3%83%93%E3%83%BC%E3%83%AB&action=edit&section=2\" \\o \"節を編集: ラインナップ​)]\nアルコール度数 (​​\\/​wiki​\\/​%E3%82%A2%E3%83%AB%E3%82%B3%E3%83%BC%E3%83%AB%E5%BA%A6%E6%95%B0\" \\o \"アルコール度数​)はすべて4.5%。 \n\t<em>コロナ</em>・エキストラ ボトル (355ml)\n\t<em>コロナ</em>・エキストラ 缶 (355ml)\n\tコロニータ・エキストラ 小瓶 (207ml)\n旧ラインナップ[編集 (​​\\/​w​\\/​index.php?title",
+        confidence: 0.8
+    }, {
+        filename: "filenameです。2",
+        passage: "passage:です2",
+        confidence: 0.8
+    }, {
+        filename: "filenameです。3",
+        passage: "passage:です3",
+        confidence: 0.8
+    }, {
+        filename: "filenameです。4",
+        passage: "passage:です4",
+        confidence: 0.8
+    }, {
+        filename: "filenameです。",
+        passage: "passage:です",
+        confidence: 0.8
+    }, {
+        filename: "filenameです。",
+        passage: "passage:です",
+        confidence: 0.8
+    },
+    ]
+}
