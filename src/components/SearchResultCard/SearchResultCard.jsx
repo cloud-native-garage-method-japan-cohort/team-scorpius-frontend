60ad2eff79741f6dd0fe56e00ecf3b801f9fd649
Hyu Okumura <Hyu.Okumura@ibm.com>
2024-08-01 13:50:33 +0900

    update: ハイライトに対応

--- src/components/SearchResultCard/SearchResultCard.jsx
+++ src/components/SearchResultCard/SearchResultCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Divider, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+
 const useStyles = makeStyles((theme) => ({
   card: {
     margin: theme.spacing(2),
@@ -9,17 +10,39 @@ const useStyles = makeStyles((theme) => ({
   filename: {
     marginTop: theme.spacing(2),
   },
+  highlight: {
+    backgroundColor: 'lightblue',
+  }
 }));
 
 const SearchResultCard = ({ passage, filename }) => {
   const classes = useStyles();
+
+  // emタグの部分をハイライトに変える関数
+  const Highlight = (text) => {
+    if (!text) return null;
+
+    const highlightedText = text.split(/(<em>.*?<\/em>)/g).map((part, index) => {
+      if (part.startsWith('<em>') && part.endsWith('</em>')) {
+        return (
+          <span key={index} className={classes.highlight}>
+            {part.replace(/<\/?em>/g, '')}
+          </span>
+        );
+      } else {
+        return <span key={index}>{part}</span>;
+      }
+    });
+
+    return <>{highlightedText}</>;
+  };
 
   return (
     <Grid item xs={12}>
       <Card className={classes.card}>
         <CardContent>
           <Typography variant="body1" component="p" align="left">
-            {passage}
+            {Highlight(passage)}
           </Typography>
           <Divider />
           <Typography className={classes.filename} variant="body2" color="textSecondary" align="left">
